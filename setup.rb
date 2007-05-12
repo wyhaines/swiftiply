58bd44e7fb08aa18a2ab14c16b2b73094859b81c
root@localhost <root@localhost>
2007-05-12 14:56:53 +0200

    Fleshed out the readme and finished the setup.rb for the release.

--- setup.rb
+++ setup.rb
@@ -20,6 +20,19 @@ Package.setup("1.0") {
 	lib("src/swiftcore/swiftiplied_mongrel.rb")
 	ri(*Dir["src/swiftcore/**/*.rb"])
 	bin "bin/swiftiply"
+	bin "bin/swiftiply_mongrel_rails"
+	#File.rename("#{Config::CONFIG["bindir"]}/mongrel_rails","#{Config::CONFIG["bindir"]}/mongrel_rails.orig")
+	bin "bin/mongrel_rails"
+
+	# Install Ramaze libs if Ramaze is installed.
+	
+	begin
+		require 'ramaze'
+		lib("src/ramaze/adapter/evented_mongrel.rb")
+		lib("src/ramaze/adapter/swiftiplied_mongrel.rb")
+	rescue LoadError
+		# Ramaze not installed
+	end
 
 #	unit_test "test/TC_Swiftiply.rb"
 	
