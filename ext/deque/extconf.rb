1ef0c56ccc09eefcc2dd01da2508c6ab2f73a58b
Kirk Haines <kirk-haines@cookpad.com>
2017-10-16 15:04:16 -0600

    Clean up a couple hanging spaces.

--- ext/deque/extconf.rb
+++ ext/deque/extconf.rb
@@ -4,12 +4,12 @@
 #
 # Copyright (C) 2006-07 by Francis Cianfrocca. All Rights Reserved.
 # Gmail: garbagecat10
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of either: 1) the GNU General Public License
 # as published by the Free Software Foundation; either version 2 of the
 # License, or (at your option) any later version; or 2) Ruby's License.
-# 
+#
 # See the file COPYING for complete licensing information.
 #
 #---------------------------------------------------------------------------
